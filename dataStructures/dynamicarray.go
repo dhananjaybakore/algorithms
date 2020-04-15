b9d7ce2868a1d3c96a8a612771a88b7bc677066c
dhananjay <dhananjaybakore@gmail.com>
2020-04-15 19:49:37 -0400

    Dyn Arr Changes

--- dataStructures/dynamicarray.go
+++ dataStructures/dynamicarray.go
@@ -1,3 +1,27 @@
 package dataStructures
 
+const SIZE = 10
+type DArr struct {
+	arr [SIZE] int
+	resizeCount int
+	currentLen int
+}
+
+
+
+
+func (dr * DArr) length() int  {
+	return dr.currentLen
+}
+
+func (dr *DArr) add(item int)  {
+	if dr.currentLen < len(dr.arr) {
+		dr.arr[dr.currentLen+1] = item
+		dr.currentLen++
+	}else{
+		// Increase the array size
+		updArr := make([]int, dr.length(), dr.length()*2)
+
+	}
+}
 
