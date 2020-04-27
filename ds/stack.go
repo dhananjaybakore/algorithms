ecb41ab0275d3cca504c0b3c742e80d923bb01bf
dhananjay <dhananjaybakore@gmail.com>
2020-04-26 22:33:59 -0400

    Stack Implementation

--- ds/stack.go
+++ ds/stack.go
@@ -1,1 +1,33 @@
 package ds
+
+type Stack struct {
+	top  *Node
+	size int
+}
+
+type Node struct {
+	data interface{}
+	next *Node
+}
+
+func (stack *Stack) Pop() (value interface{}) {
+	if stack.size > 0 {
+		value, stack.top = stack.top.data, stack.top.next
+		stack.size--
+		return
+	}
+	return nil
+}
+
+func (stack *Stack) Push(item interface{}) {
+	stack.top = &Node{item, stack.top}
+	stack.size++
+}
+
+func (stack *Stack) Peek() interface{} {
+	return stack.top.data
+}
+
+func (stack *Stack) IsEmpty() bool {
+	return stack.top == nil
+}
---
+++ ds/stack_test.go
@@ -0,0 +1,44 @@
+package ds
+
+import (
+	"testing"
+)
+
+func TestPushAndPeekOperation(t *testing.T) {
+	var stack Stack
+	stack.Push(2)
+	stack.Push(3)
+	stack.Push(4)
+	stack.Push(5)
+	ele := stack.Peek()
+	if ele != 5 && !stack.IsEmpty() {
+		t.Errorf("elements not inserted properly to the stack, top ele should be:5 but got %v", ele)
+	}
+}
+
+func TestPopOperation(t *testing.T) {
+	var stack Stack
+	stack.Push(2)
+	stack.Push(3)
+	stack.Push(4)
+	stack.Push(5)
+	topEle := stack.Peek()
+	if topEle != 5 && !stack.IsEmpty() {
+		t.Errorf("the popped element should be: %v but got:%v", 5, topEle)
+	}
+	ele := stack.Pop()
+	if ele != 5 && !stack.IsEmpty() {
+		t.Errorf("the top element should be: %v but got: %v", 5, ele)
+	}
+}
+
+func TestPopOnOneElementStack(t *testing.T) {
+	var stack Stack
+	stack.Push(2)
+	stack.Pop()
+}
+
+func TestPopEmptyStack(t *testing.T) {
+	var stack Stack
+	stack.Pop()
+}
